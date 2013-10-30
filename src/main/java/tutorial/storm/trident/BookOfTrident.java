3d1674f9a213fb64418c64094d3d1462719af343
Enno Shioji <eshioji@gmail.com>
2013-10-30 16:20:17 +0000

    Illustrate use of external state and a snapshot aggregation as well as chained aggregation

--- src/main/java/tutorial/storm/trident/BookOfTrident.java
+++ src/main/java/tutorial/storm/trident/BookOfTrident.java
@@ -4,23 +4,22 @@ import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.LocalDRPC;
 import backtype.storm.generated.StormTopology;
+import backtype.storm.task.IMetricsContext;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
-import backtype.storm.utils.DRPCClient;
 import com.google.common.collect.ImmutableList;
 import org.apache.commons.collections.MapUtils;
-import storm.trident.operation.builtin.FilterNull;
-import storm.trident.operation.builtin.Sum;
+import storm.trident.operation.builtin.*;
 import storm.trident.spout.IBatchSpout;
+import storm.trident.state.State;
+import storm.trident.state.StateFactory;
 import storm.trident.testing.FeederBatchSpout;
-import trident.memcached.MemcachedState;
+import tutorial.storm.trident.operations.DivideAsDouble;
 import tutorial.storm.trident.operations.FilterByRegex;
 import tutorial.storm.trident.operations.Split;
 import storm.trident.TridentState;
 import storm.trident.TridentTopology;
 import storm.trident.operation.*;
-import storm.trident.operation.builtin.Count;
-import storm.trident.operation.builtin.MapGet;
 import storm.trident.testing.MemoryMapState;
 import storm.trident.tuple.TridentTuple;
 import tutorial.storm.trident.testutil.FakeTweetGenerator;
@@ -28,7 +27,7 @@ import tutorial.storm.trident.testutil.FakeTweetsBatchSpout;
 import tutorial.storm.trident.testutil.Utils;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -45,18 +44,93 @@ import java.util.Map;
 public class BookOfTrident {
 
     public static void main(String[] args) throws Exception {
-        FakeTweetGenerator fakeTweets = new FakeTweetGenerator();
-
         // Storm can be run locally for testing purposes
         Config conf = new Config();
         LocalCluster cluster = new LocalCluster();
 
 
-        // Learn about Trident's basic primitives
-        cluster.submitTopology("basic_primitives", conf, basicPrimitives(new FakeTweetsBatchSpout()));
+         // Learn about Trident's basic primitives
+//        learnBasicPrimitives(conf, cluster);
 
 
-        // Learn how to use Trident State and DRPC
+         // Learn how to use Trident State and DRPC
+//        learnStateAndDRPC(conf, cluster);
+
+         // Learn how to use external datasources
+        learnExternalState(conf, cluster);
+
+    }
+
+    private static void learnExternalState(Config conf, LocalCluster cluster) {
+        FakeTweetGenerator fakeTweets = new FakeTweetGenerator();
+        FeederBatchSpout testSpout = new FeederBatchSpout(ImmutableList.of("id", "text", "actor", "location", "date"));
+        LocalDRPC drpc = new LocalDRPC();
+        cluster.submitTopology("external_state_drpc", conf, externalState(drpc, testSpout));
+
+        // You can use FeederBatchSpout to feed know values to the topology. Very useful for tests.
+        testSpout.feed(fakeTweets.getNextTweetTuples("ted"));
+        testSpout.feed(fakeTweets.getNextTweetTuples("ted"));
+        testSpout.feed(fakeTweets.getNextTweetTuples("mary"));
+        testSpout.feed(fakeTweets.getNextTweetTuples("jason"));
+
+        System.out.println(drpc.execute("age_stats", ""));
+        System.out.println("OK");
+
+    }
+
+    private static StormTopology externalState(LocalDRPC drpc, FeederBatchSpout spout) {
+        TridentTopology topology = new TridentTopology();
+
+        // You can reference existing data sources as well.
+        // Here we are mocking up a "database"
+        StateFactory stateFactory = new StateFactory() {
+            @Override
+            public State makeState(Map conf, IMetricsContext metrics, int partitionIndex, int numPartitions) {
+                MemoryMapState<Integer> name_to_age = new MemoryMapState<Integer>("name_to_age");
+                // This is a bit hard to read but it's just pre-populating the state
+                List<List<Object>> keys = getKeys("ted", "mary", "jason", "tom", "chuck");
+                name_to_age.multiPut(keys, ImmutableList.of(32, 21, 45, 52, 18));
+                return name_to_age;
+            }
+        };
+        TridentState nameToAge =
+                topology.newStaticState(stateFactory);
+
+        // Let's setup another state that keeps track of actor's appearance counts per location
+        TridentState countState =
+                topology
+                        .newStream("spout", spout)
+                        .groupBy(new Fields("actor","location"))
+                        .persistentAggregate(new MemoryMapState.Factory(), new Count(), new Fields("count"));
+
+        // Now, let's calculate the average age of actors seen
+        topology
+                .newDRPCStream("age_stats", drpc)
+                .stateQuery(countState, new TupleCollectionGet(), new Fields("actor", "location"))
+                .stateQuery(nameToAge, new Fields("actor"), new MapGet(), new Fields("age"))
+                .each(new Fields("actor","location","age"), new Debug())
+                .groupBy(new Fields("location"))
+                .chainedAgg()
+                .aggregate(new Count(), new Fields("count"))
+                .aggregate(new Fields("age"), new Sum(), new Fields("sum"))
+                .chainEnd()
+                .each(new Fields("sum", "count"), new DivideAsDouble(), new Fields("avg"))
+                .project(new Fields("location", "count", "avg"))
+        ;
+
+        return topology.build();
+    }
+
+    private static List<List<Object>> getKeys(String... names) {
+        List<List<Object>> ret = new ArrayList<List<Object>>();
+        for (String name : names) {
+            ret.add(ImmutableList.<Object>of(name));
+        }
+        return ret;
+    }
+
+    private static void learnStateAndDRPC(Config conf, LocalCluster cluster) throws IOException {
+        FakeTweetGenerator fakeTweets = new FakeTweetGenerator();
         FeederBatchSpout testSpout = new FeederBatchSpout(ImmutableList.of("id", "text", "actor", "location", "date"));
         LocalDRPC drpc = new LocalDRPC();
         cluster.submitTopology("state_drpc", conf, stateAndDRPC(drpc, testSpout));
@@ -76,7 +150,10 @@ public class BookOfTrident {
         // You can use a client library to make calls remotely
 //        DRPCClient client = new DRPCClient("drpc.server.location", 3772);
 //        System.out.println(client.execute("ping", "ping pang pong"));
+    }
 
+    private static void learnBasicPrimitives(Config conf, LocalCluster cluster) throws IOException {
+        cluster.submitTopology("basic_primitives", conf, basicPrimitives(new FakeTweetsBatchSpout()));
     }
 
 
